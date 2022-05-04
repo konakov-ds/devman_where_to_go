76fd7df10d139f1ddbeeb7eb746e3acdad652f78
konakov-ds <konakov-ds@yandex.ru>
2022-05-04 16:19:27 +0300

    Add frontend

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -21,7 +21,6 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -124,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.template import loader
 
