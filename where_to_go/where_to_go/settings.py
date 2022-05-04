ef49b43feaf43b47495e9f42d946078f1aee8743
konakov-ds <konakov-ds@yandex.ru>
2022-05-04 08:54:28 +0300

    Add blank start page

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 from environs import Env
 
 env = Env()
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
