5ce8daef0073f95347b8d15a4635c4c6466a1efd
konakov-ds <konakov-ds@yandex.ru>
2022-05-05 11:37:19 +0300

    Add Place, PlaceImage models

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
-# Create your models here.
-print('hello')
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField(blank=True)
+    lng = models.FloatField(blank=True)
+    lat = models.FloatField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PlaceImage(models.Model):
+    img = models.ImageField(verbose_name='img')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'places'
+    'places',
+    'django.contrib.gis'
 ]
 
 MIDDLEWARE = [
